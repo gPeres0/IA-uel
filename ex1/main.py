062389b0d2f905b68f035cb3ae24948911c913dd
gPeres0 <gbperes.emp@gmail.com>
2025-04-17 15:09:08 -0300

    add: Adicionada função que redefine índices do DataFrame filtrado

--- ex1/main.py
+++ ex1/main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-from haversine import haversine
+import random
+import pandas as pd # type: ignore
+from haversine import haversine # type: ignore
 
 
 worldcities = pd.read_csv("./worldcities.csv")
@@ -8,13 +9,18 @@ mask_pr = df.admin_name == 'Paraná' # Cria uma máscara (boolean) dos valores q
 
 def main():
     pr_cities = df[mask_pr] # Usa a máscara para exibir os valores correspondentes
-    #print(pr_cities)
+    pr_cities.index = pd.RangeIndex(start=0, stop=190, step=1) # Redefine o índice das cidades por questões de praticidade
+    print(pr_cities)
+
+    n = int(random.uniform(2, 6.9))
+#    print(n)
+
     # Cria tuple com as coordenadas de teste (curitiba e londrina)
     curitiba = (pr_cities.at[453, "lat"], pr_cities.at[453, "lng"])
     londrina = (pr_cities.at[1328, "lat"], pr_cities.at[1328, "lng"])
 
     # Imprime o resultado do cálculo de Haversine entre os tuples de teste criados
-    print(haversine(curitiba, londrina))
+#    print(haversine(curitiba, londrina))
 
     
 
